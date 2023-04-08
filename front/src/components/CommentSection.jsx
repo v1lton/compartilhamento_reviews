49cf5d4bfe7786dea24efebbf8b329c2b050d45b
agsa2 <agsa0108@gmail.com>
2023-04-08 11:04:00 -0300

    Delet comment function added

--- front/src/components/CommentSection.jsx
+++ front/src/components/CommentSection.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { List, Input, Button, Avatar, Popover } from 'antd';
-import { UserOutlined, EllipsisOutlined } from '@ant-design/icons';
+import { UserOutlined, DeleteOutlined } from '@ant-design/icons';
 import User from "./User";
+import moment from 'moment';
+
 
 const { TextArea } = Input;
+const timeStamp = moment().format('HH:mm:ss');
+
 
 
 const CommentSection = () => {
@@ -11,6 +15,11 @@ const CommentSection = () => {
   const [comments, setComments] = useState([]);
 
   const handleCommentSubmit = () => {
+    const now = new Date().toLocaleString();
+    const newComment = {
+      text: comment,
+      timestamp: moment().format('HH:mm:ss')
+    };
     setComments([...comments, comment]);
     setComment('');
   };
@@ -50,8 +59,8 @@ const handlePopoverVisibleChange = (visible, index) => {
             borderRadius: '999px',
             top: '1px',
             right: '-4px' 
-        }} onClick={handleCommentSubmit}>
-          Send
+          }} onClick={handleCommentSubmit}>
+           Send
         </Button>
       </div>
       <List
@@ -60,34 +69,44 @@ const handlePopoverVisibleChange = (visible, index) => {
           <List.Item
              actions={[
                 <Popover
-                content={
-                deletingComment === index ? (
-                    <>
-                    <Button danger onClick={() => setDeletingComment(null)}>
-                     Cancel
-                    </Button>
-                    <Button
-                        type="primary"
-                        danger
-                        onClick={() => handleDeleteComment(index)}
-                        >
-                         Confirm   
+                  content={
+                    deletingComment === index ?  (
+                    <></>
+                    ) : (
+                      <>
+                        <Button danger onClick={() => setDeletingComment(null)}>
+                          Cancel
                         </Button>
-                    </>
-                ) : (
-                    <Button>Delete</Button>
-                )
-            }
-            trigger="click"
+                        <Button
+                          type="primary"
+                          danger
+                          onClick={() => handleDeleteComment(index)}
+                          >
+                          Confirm   
+                        </Button>
+                      </>
+                    )
+                  }
+                  trigger="click"
+                  >   
+                  <Button danger onClick={() => setDeletingComment(null)}>
+                    <DeleteOutlined />
+                  </Button>
+                </Popover>,
+                
+              ]}
             >
-            <EllipsisOutlined />
-            </Popover>,
-             ]}
-             >
             <List.Item.Meta
-              avatar={<Avatar size={32} src={<User imgSrc={item.imgSrc} />} />}
-              title={<a href="#example">{item}</a>}
-              style={{ display: 'flex', alignItems: 'left', justifyContent: 'flex-start' }}
+              avatar={
+                <Avatar size={32} src={<User imgSrc={item.imgSrc} />} />}
+              title={
+                <>
+                  <a href="#example">{item}</a>
+                  <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', marginLeft: '8px' }}>
+                    {item.timestamp}</span>
+                </>
+              }
+              style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
             />
           </List.Item>
         )}
