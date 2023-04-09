a9a80038893f25f2afb466db3443f3d4dd7b84de
Wilton Silva <wrs@cin.ufpe.br>
2023-04-09 12:40:39 -0300

    WIP: Atualiza testes de Users controller

--- back/test/controllers/users_controller_test.rb
+++ back/test/controllers/users_controller_test.rb
@@ -8,6 +8,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get users_url, as: :json
     assert_response :success
+    assert_equal User.all.count, response.parsed_body.count
   end
 
   test "should create user" do
@@ -28,8 +29,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_difference("User.count") do
       post users_url, params: body, as: :json
     end
+    created_user = response.parsed_body
 
     assert_response :created
+    assert_equal body[:user][:username], created_user["username"]
+    assert_equal body[:user][:email], created_user["email"]
+    assert_equal body[:user][:surname], created_user["surname"]
+    assert_equal body[:user][:pronouns], created_user["pronouns"]
+    assert_equal body[:user][:country], created_user["country"]
   end
 
   test "should show user" do
